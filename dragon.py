f1567201c8015a1296b4151c1089f615c9b0c901
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-03-11 12:59:04 -0700

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -8,16 +8,20 @@ import random
 import Entity
 
 class Dragon(Entity.Entity):
+    # Creates dragon class
     def __init__(self, name, max_hp):
+        #Initializes dragon with a name and max health
         super().__init__(name, max_hp)
         
 
     def basic_attack(self, hero):
+        # Dragon conducts basic attack that damages the hero and returns a string
         damage = random.randint(2, 5)
         hero.take_damage(damage)
         return f"{self.name} attacks {hero.name} for {damage} damage!"
 
     def special_attack(self, hero):
+        # Dragon conducts special attack that damages the hero and returns a string
         damage = random.randint(3, 7)
         hero.take_damage(damage)
         return f"{self.name} uses a special attack on {hero.name} for {damage} damage!"
