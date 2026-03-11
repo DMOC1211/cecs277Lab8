d33d5ad722802d6e0003dd3b220bb609f3cc05e7
Polyploy05 <jakada005@gmail.com>
2026-03-10 22:11:04 -0700

    Update hero.py

--- hero.py
+++ hero.py
@@ -5,6 +5,19 @@ Group: 13
 Description:
 '''
 
-
 import random
-from entity Import Entity
+import Entity 
+
+class Hero(Entity.Entity):
+    def __init__(self, name, max_hp):
+        super().__init__(name, max_hp)
+
+    def sword_attack(self, dragon):
+        damage = random.randint(1, 6) + random.randint(1, 6)
+        dragon.take_damage(damage)
+        return f"{self.name} attacks {dragon.name} with a sword for {damage} damage!"
+    
+    def arrow_attack(self, dragon):
+        damage = random.randint(1, 12)
+        dragon.take_damage(damage)
+        return f"{self.name} attacks {dragon.name} with an arrow for {damage} damage!"
