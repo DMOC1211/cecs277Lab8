a39eb0d41d409352bb40d5c03513029583f18be1
Polyploy05 <jakada005@gmail.com>
2026-03-11 12:47:15 -0700

    Update flying.py

--- flying.py
+++ flying.py
@@ -7,23 +7,23 @@ Description:
 import random 
 from dragon import Dragon
 
-class FlyingDragon(Dragon):
+class FlyingDragon(Dragon.Dragon):
 # A dragon that can swoop a limited number of times
-  def __init__(self,name, hp):
+    def __init__(self, name, hp):
     # Initializes FlyingDragon with a name, HP and swoop count, flying dragon starts with 2 swoops
-    super().__init__(name, hp)
-    self._swoops = 2
+        super().__init__(name, hp)
+        self._swoops = 2
 
-  def special_attack(self, hero):
+    def special_attack(self, hero):
     # Swoop attack deals 5-9 damage, but only if swoops remain, if there are no swoops remaining, it deals 0 damage and prints a message
-    if self._swoops > 0:
-      dmg = random.randint(5,9)
-      hero.take_damage(dmg)
-      self._swoops -= 1
-      return f"{self.name} swoops down on {hero.name} folr {dmg} damage!"
-    else:
-      return f"{self.name} tries to swoop but has no swoops left!"
+        if self._swoops > 0:
+            dmg = random.randint(5,9)
+            hero.take_damage(dmg)
+            self._swoops -= 1
+            return f"{self.name} swoops down on {hero.name} for {dmg} damage!"
+        else:
+            return f"{self.name} tries to swoop but has no swoops left!"
 
-  def __str__(self):
-    # Return dragon's name, HP and remaining swoops
-    return f"{self.name}: {self.hp} HP (Swoops: {self._swoops})"
+    def __str__(self):
+        # Return dragon's name, HP and remaining swoops
+        return f"{self.name}: {self.health} HP (Swoops: {self._swoops})"
