ae2a20caec7776e574c1d33d08b7afae41b62919
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-03-10 22:31:52 -0700

    Update flying.py

--- flying.py
+++ flying.py
@@ -4,3 +4,26 @@ Date: 03/10/26
 Group: 13
 Description:
 '''
+import random 
+from dragon Import Dragon
+
+class FlyingDragon(Dragon):
+
+  def __init__(self,name, hp):
+
+    super().__init__(name, hp)
+    self._swoops = 2
+
+  def special_attack(self, hero):
+
+    if self._swoops > 0:
+      dmg = random.randint(5,9)
+      hero.take_damage(dmg)
+      self._swoops -= 1
+      return f"{self.name} swoops down on {hero.name} folr {dmg} damage!"
+    else:
+      return f"{self.name} tries to swoop but has no swoops left!"
+
+  def __str__(self):
+
+    return f"{self.name}: {self.hp} HP (Swoops: {self._swoops})"
