b8e11a34cab3c739ed273f2cfebc9b9444bdd2e4
Polyploy05 <jakada005@gmail.com>
2026-03-10 22:11:28 -0700

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -4,3 +4,20 @@ Date: 03/10/26
 Group: 13
 Description:
 '''
+import random
+import Entity
+
+class Dragon(Entity.Entity):
+    def __init__(self, name, max_hp):
+        super().__init__(name, max_hp)
+        
+
+    def basic_attack(self, hero):
+        damage = random.randint(2, 5)
+        hero.take_damage(damage)
+        return f"{self.name} attacks {hero.name} for {damage} damage!"
+
+    def special_attack(self, hero):
+        damage = random.randint(3, 7)
+        hero.take_damage(damage)
+        return f"{self.name} uses a special attack on {hero.name} for {damage} damage!"
